abc6073d2a5e1256eb5aefb3141f91605c3662f5
DaianCosta <desenvolvimentodaian@gmail.com>
2022-04-19 08:10:03 -0300

    ädd

---
+++ src/main/java/compositepattern/ProductComponent.java
@@ -0,0 +1,17 @@
+package compositepattern;
+
+abstract class ProductComponent {
+
+    String name;
+    Double price = 0.0;
+
+    public abstract Double getTotal();
+
+    public void add(final ProductComponent productComponent) {
+        throw new UnsupportedOperationException("Feature is not implemented yet");
+    }
+
+    public void remove(final ProductComponent productComponent) {
+        throw new UnsupportedOperationException("Feature is not implemented yet");
+    }
+}
